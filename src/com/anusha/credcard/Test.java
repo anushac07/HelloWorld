1ca51a5dde99478e6d978b6dfeff562b2826fa5b
anushac007 <chandrashekar.anushac7@gmail.com>
2020-01-26 22:21:46 +0530

    committing class files

---
+++ src/com/anusha/credcard/Test.java
@@ -0,0 +1,15 @@
+package com.anusha.credcard;
+
+
+public class Test {
+
+	
+		 public static void main (String[] args)
+		   {
+			   String cardNo = "79927398713";
+			   System.out.println(CardValidapi.checkLuhn(cardNo));
+			   
+		   }
+	}
+
+
